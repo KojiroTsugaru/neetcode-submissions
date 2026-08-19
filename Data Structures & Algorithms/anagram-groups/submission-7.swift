7ba1b8851afab861d0aaca54a00b3b72411d7ca2
Kojiro Tsugaru <93096898+KojiroTsugaru@users.noreply.github.com>
2026-08-18 18:48:36 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var anagrams = [[Int]: [String]]()
+
+        for str in strs {
+            let chars = Array(str)
+            var counts = [Int](repeating: 0, count: 26)
+
+            for c in chars {
+                counts[Int(c.asciiValue!) - 97] += 1
+            }
+            
+            anagrams[counts, default: []].append(str)
+        }
+
+        return Array(anagrams.values)
+    }
+}
