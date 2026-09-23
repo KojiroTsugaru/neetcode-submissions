a84b6ca76bbbb3f6fc6e3039dfdcec647eaa0964
Kojiro Tsugaru <93096898+KojiroTsugaru@users.noreply.github.com>
2026-09-22 20:32:36 -0700

    Add: squares-of-a-sorted-array - submission-1

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-1.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func sortedSquares(_ nums: [Int]) -> [Int] {
+        let n = nums.count
+        var l = 0
+        var r = n - 1
+        var res = Array(repeating: 0, count: n)
+        var resIndex = n - 1
+
+        while l <= r {
+            if abs(nums[l]) > abs(nums[r]) {
+                res[resIndex] = nums[l] * nums[l]
+                l += 1
+            } else {
+                res[resIndex] = nums[r] * nums[r]
+                r -= 1
+            }
+            resIndex -= 1
+        }
+
+        return res
+    }
+}
